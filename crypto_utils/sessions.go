62fb906f2f513f62fc48ce5e8445ea1dfd2053a9
nikolas batzios <info@technikos.io>
2024-08-31 21:06:48 +0100

    sessions

---
+++ crypto_utils/sessions.go
@@ -0,0 +1,86 @@
+package crypto_utils
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"io"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/nmpatzios/go-utils/logger"
+)
+
+var (
+	KeyGenerator = []byte("NcRfUjXn2r5u8x/A")
+	mySigningKey = []byte("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJjbGllbnQiOiJKaXNjIiwiZXhwIjowfQ.CYwly1IRD_F9ptpSx8OJQVL2jgwMpkxlR5HheQLF9us")
+)
+
+func GenerateJWT(username, userId string) (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	claims := token.Claims.(jwt.MapClaims)
+
+	claims["authorized"] = true
+	claims["client"] = username
+	claims["exp"] = 0
+	claims["user_id"] = userId
+	tokenString, err := token.SignedString(mySigningKey)
+
+	if err != nil {
+
+		return "", fmt.Errorf("Something Went Wrong: %s", err.Error())
+	}
+
+	return tokenString, nil
+}
+
+func EncryptPass(key []byte, text string) string {
+	// key := []byte(keyText)
+	plaintext := []byte(text)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		logger.Error("", err)
+	}
+
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		logger.Error("", err)
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
+
+	// convert to base64
+	return base64.URLEncoding.EncodeToString(ciphertext)
+}
+
+// decrypt from base64 to decrypted string
+func DecryptPass(key []byte, cryptoText string) string {
+	ciphertext, _ := base64.URLEncoding.DecodeString(cryptoText)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		logger.Error("", err)
+	}
+
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	if len(ciphertext) < aes.BlockSize {
+		logger.Error("ciphertext too short", err)
+	}
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	stream := cipher.NewCFBDecrypter(block, iv)
+
+	// XORKeyStream can work in-place if the two arguments are the same.
+	stream.XORKeyStream(ciphertext, ciphertext)
+
+	return fmt.Sprintf("%s", ciphertext)
+}
